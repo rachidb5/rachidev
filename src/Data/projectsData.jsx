204b8f2f8c1e59fcd7b443be599fe52e39c0a2ff
rachidb5 <jordan.rachid@gmail.com>
2022-12-20 10:18:46 -0300

    completa traducao

--- src/Data/projectsData.jsx
+++ src/Data/projectsData.jsx
@@ -3,7 +3,7 @@ import colorguess from '../assets/colorguess.png';
 import meetpets from '../assets/meetpets.png';
 import jokenpo from '../assets/jokenpo.jpeg'
 
-const projects = [
+export const projects = [
     {
         projectName: "Blackjack",
         description: "Classico jogo de cassino também conhecido com 21 feito em react e usando uma API chamada deck of cards para renderizar as cartas",
@@ -37,5 +37,60 @@ const projects = [
         index: 'flex-lg-row' 
     }
 ]
+export const projectsEn = [
+    {
+        projectName: "Blackjack",
+        description: "Classic cassino game also known as 21 made in react e using a public API called deck of cards to show the cards and get the values",
+        projectImg: blackjack,
+        projectLink: 'https://blackjack-steel.vercel.app/',
+        repository: 'https://github.com/rachidb5/Blackjack',
+        index: 'flex-lg-row-reverse'
+    },
+    {
+        projectName: "ColorGuess",
+        description: "The main idea of this game is to play with rgb code. The user gets a rgb code and color options to guess which these colors correspond to the given code. The used technologies are html, css e vanilla JS(without frameworks)",
+        projectImg: colorguess,
+        projectLink: 'https://color-guess-sooty.vercel.app/',
+        repository: 'https://github.com/rachidb5/color-guess',
+        index: 'flex-lg-row' 
+    },
+    {
+        projectName: "MeetPets",
+        description: "Fictional event of animal adoption created with end of expand e complement my portfolio. A API was created with NodeJS, MongoDB and express in which the user sign up and get a QRcode that is the invitation to the event",
+        projectImg: meetpets,
+        projectLink: 'https://meetpets-qrcode.herokuapp.com/',
+        repository: 'https://github.com/rachidb5/meetpets-back',
+        index: 'flex-lg-row-reverse' 
+    },
+    {
+        projectName: "JoKenPo",
+        description: "The traditional japonese game also known as rock, scissor, paper for web using HTML, CSS, javascript and VueJS. It was my first contact with the framework.",
+        projectImg: jokenpo,
+        projectLink: 'https://jokenpo-vue.vercel.app/',
+        repository: 'https://github.com/rachidb5/jokenpo-vue',
+        index: 'flex-lg-row' 
+    }
+]
 
-export default projects;
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+;
--- src/Pages/Projetos.jsx
+++ src/Pages/Projetos.jsx
@@ -6,17 +6,24 @@ import { useContext } from 'react';
 import DropdownHeader from '../Components/DropdownHeader';
 import Footer from '../Components/Footer';
 import ProjectCard from '../Components/ProjectCard';
-import Data from '../Data/projectsData';
+import { projects, projectsEn } from '../Data/projectsData';
 import { Carousel } from 'react-responsive-carousel';
 
 function Main() {
-  const { menu }= useContext(Context);
-  console.log(Data)
+  const { menu, language }= useContext(Context);
   return (
     <div className="bg">
 
     <h1 className="titulo-listas text-light" id="tecnologias">Projetos</h1>
-      {Data.map((p) => <ProjectCard 
+      {language ? projectsEn.map((p) => <ProjectCard 
+         index={p.index}
+         projectName={p.projectName}
+         description={p.description}
+         projectImg={p.projectImg}
+         projectLink={p.projectLink}
+         repository={p.repository}
+         />
+      ):projects.map((p) => <ProjectCard 
          index={p.index}
          projectName={p.projectName}
          description={p.description}
